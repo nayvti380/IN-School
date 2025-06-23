894f986dffade122855326c401ab86045596ff4a
Nayara V <nayaravti.380@gmail.com>
2025-06-22 22:52:17 -0300

    Add files via upload

---
+++ Lógica de Programação/Aula 01/aritmeticos.py
@@ -0,0 +1,12 @@
+# Operadores Aritmeticos
+
+n1 = 5
+n2 = 2
+
+print(n1 + n2) # Soma
+print(n1 - n2) # Subtração
+print(n1 * n2) # Multiplicação
+print(n1 / n2) # Divisão
+print(n1 ** n2) # Potenciação
+print(n1 // n2) # Divisão inteira
+print(n1 % n2) # Resto da divisão (Modulo)
---
+++ Lógica de Programação/Aula 01/entrada.py
@@ -0,0 +1,6 @@
+# Entrada (input)
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+# Saída (f-string)
+print(f'Meu nome é {nome} e tenho {idade} anos')
