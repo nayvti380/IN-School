75573b9800df7ea864457261cb8a1c55ff2455b1
Nayara V <nayaravti.380@gmail.com>
2025-06-22 22:31:57 -0300

    Add files via upload

---
+++ Aula 01/casting.py
@@ -0,0 +1,8 @@
+# Casting é a operação de converter um tipo para outro!
+
+# Aqui estamos convertendo uma string para float
+valor1 = float(input('Digite um numero decimal: '))
+
+# Aqui estamos convertendo uma string para um int
+valor2 = input('Digite um numero inteiro')
+valor2 = int(valor2) # Reatribuindo.
---
+++ Aula 01/ex03.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite o 1º numero: '))
+n2 = int(input('Digite o 2º numero: '))
+
+resultado = n1 * n2
+
+print(f'O produto (multiplicação) é {resultado}')
