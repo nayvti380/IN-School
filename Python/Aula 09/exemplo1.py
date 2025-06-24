3ee4755d589b1158a23ea123a68d3b2dbc012b0a
Nayara V <nayaravti.380@gmail.com>
2025-06-24 13:30:26 -0300

    Add files via upload

---
+++ Python/Aula 09/exemplo1.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# Markdown
+st.write('''
+# Olá Mundo!
+
+Meu **Primeiro** Site
+
+## Lista de Tecnologias
+
+- Python
+- [Streamlit](https://streamlit.io/)
+''')
---
+++ Python/Aula 09/exemplo2.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Markdown
+f'''
+# Hello World
+
+Meu **Primeiro** Site
+
+'''
+
+st.image('./matrix.jpg')
+
+# Imagens
+st.title('Eu Mesmo')
+st.image('https://www.github.com/davi-lucciola.png', 'Davi Lucciola Foto', width=700)
+
